0bfd6e1ec9183dfc6b6af9c43c1aacce5fe810d9
Namhyun, Gu <mnhan0403@gmail.com>
2019-05-08 21:04:13 +0900

    Add register/unregister user via Firebase

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-const TelegramBot = require('node-telegram-bot-api');
-const request = require('request');
-const cheerio = require('cheerio');
+const TelegramBot = require("node-telegram-bot-api");
+const request = require("request");
+const cheerio = require("cheerio");
+const firebaseAdmin = require("firebase-admin");
 
 const token = process.env.TELEGRAM_TOKEN;
+const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY;
 
 const bot = new TelegramBot(token, {
   polling: true
 });
 
-var chatIds = new Set();
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert({
+    projectId: "contest-crawler-bot",
+    clientEmail:
+      "firebase-adminsdk-ib6rr@contest-crawler-bot.iam.gserviceaccount.com",
+    privateKey: firebasePrivateKey
+  }),
+  databaseURL: "https://contest-crawler-bot.firebaseio.com"
+});
+
+const database = firebaseAdmin.database();
+const userRef = database.ref("/user");
 
 function parseContest(category, callback) {
   const targetOptions = {
-    uri: 'https://www.thinkcontest.com/m/Contest/CateField.html',
+    uri: "https://www.thinkcontest.com/m/Contest/CateField.html",
     qs: {
       c: category
     }
-  }
+  };
 
   request(targetOptions, (error, response, body) => {
     if (error) callback(error, null);
@@ -24,17 +37,17 @@ function parseContest(category, callback) {
     const $ = cheerio.load(body);
 
     let results = [];
-    const list = $('.contest-list').children();
+    const list = $(".contest-list").children();
     for (let i = 0; i < list.length; i++) {
       const element = list.eq(i);
 
-      const url = element.find('a').attr('href');
-      const title = element.find('h3 > span').text();
-      const organizer = element.find('.organizer').text();
-      const timeLimit = element.find('.time-limit').text();
-      const views = element.find('.views > .num').text();
-      const status = element.find('.status').text();
-      const dday = element.find('span.d-day').text();
+      const url = element.find("a").attr("href");
+      const title = element.find("h3 > span").text();
+      const organizer = element.find(".organizer").text();
+      const timeLimit = element.find(".time-limit").text();
+      const views = element.find(".views > .num").text();
+      const status = element.find(".status").text();
+      const dday = element.find("span.d-day").text();
 
       results.push({
         url,
@@ -51,35 +64,57 @@ function parseContest(category, callback) {
 }
 
 bot.onText(/\/start/, (msg, match) => {
-  chatIds.add(msg.chat.id);
+  const chatId = msg.chat.id;
+  let newUserRef = userRef.push();
+  newUserRef.set({
+    chatId
+  });
+  bot.sendMessage(
+    chatId,
+    "알림 목록에 추가되었습니다. /cancel 로 목록에서 삭제할 수 있습니다."
+  );
 });
 
 bot.onText(/\/cancel/, (msg, match) => {
   const chatId = msg.chat.id;
-  if (chatIds.has(chatId)) {
-    chatIds.delete(chatId);
-  }
+  userRef.once("value").then(snapshot => {
+    snapshot.forEach(child => {
+      const key = child.key;
+      const val = child.val();
+
+      if (val.chatId == chatId) {
+        userRef.child(key).remove();
+        bot.sendMessage(
+          chatId,
+          "알림 목록에서 삭제되었습니다. /start 로 다시 시작할 수 있습니다."
+        );
+        return true;
+      }
+    });
+  });
 });
 
 bot.onText(/\/list/, (msg, match) => {
   parseContest(12, (err, results) => {
-    let message = '';
+    let message = "";
     results.forEach(result => {
-      message += `[${result.title}](https://www.thinkcontest.com${result.url})\n`;
+      message += `[${result.title}](https://www.thinkcontest.com${
+        result.url
+      })\n`;
       message += `${result.organizer}\n\n`;
       message += `기간: ${result.timeLimit}`;
-      if (result.dday != '') {
+      if (result.dday != "") {
         message += ` (${result.dday})\n`;
       } else {
-        message += '\n';
+        message += "\n";
       }
       message += `상태: ${result.status}\n`;
       message += `조회수: ${result.views}\n`;
-      message += '\n';
+      message += "\n";
     });
 
     bot.sendMessage(msg.chat.id, message, {
-      parse_mode: 'Markdown'
+      parse_mode: "Markdown"
     });
   });
 });
